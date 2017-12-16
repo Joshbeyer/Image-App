a214ecab4a0195511a998917315d7d2e0deca27b
Josh Beyer <mydevaccs@gmail.com>
2017-12-15 21:11:59 -0600

    Update token.js

--- models/token.js
+++ models/token.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 const tokenSchema = new mongoose.Schema({
   _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   token: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now, expires: 43200 }
+  createdAt: { type: Date, required: true, default: Date.now, expires: 86400 }
   // add expiration
 });
 
